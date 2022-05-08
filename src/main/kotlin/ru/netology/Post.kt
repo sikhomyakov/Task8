b5f0ac04e7c460bcc215a8b1902e2a73da2e72d2
sikhomyakov@gmail.com <Supernova2022>
2022-05-08 14:49:19 +0500

    Task6.1.1

--- src/main/kotlin/ru/netology/Post.kt
+++ src/main/kotlin/ru/netology/Post.kt
@@ -1,30 +1,27 @@
 package ru.netology
 
-
-// для быстрой проверки работоспособности кода, большую часть полей закомментил
-
 data class Post(
-    internal var id: Int = 0,
-//    val ownerId: Int,
-//    val fromId: Int,
-//    val createdBy: Int,
-//    val date: Int,
+    var id: Int = 0,
+    val ownerId: Int,
+    val fromId: Int,
+    val createdBy: Int,
+    val date: Int,
     val text: String,
-//    val replyOwner8Id: Int,
-//    val replyPostId: Int,
-//    val friendsOnly: Boolean,
-//    val comments: Objects,
-//    val copyright: String,
-//    val likes: Int,
-//    val repost: Objects,
-//    val views: Objects,
-//    val postType: String,
-//    val signerId: Int,
-//    val canPin: Boolean,
-//    val canDelete: Boolean,
-//    val canEdit: Boolean,
-//    val isPinned: Boolean,
-//    val markedAsAds: Boolean,
+    val replyOwnerId: Int,
+    val replyPostId: Int,
+    val friendsOnly: Boolean,
+    val comments: String,
+    val copyright: String,
+    val likes: Int?,
+    val repost: Post?,
+    val views: Int?,
+    val postType: String,
+    val signerId: Int,
+    val canPin: Boolean,
+    val canDelete: Boolean,
+    val canEdit: Boolean,
+    val isPinned: Boolean,
+    val markedAsAds: Boolean,
     val isFavorite: Boolean,
-//    val postponedId: Int
+    val postponedId: Int
 )
